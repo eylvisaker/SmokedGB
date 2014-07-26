e4b6b81c76dc736fff2300f9a0df904a79d112c2
Erik Ylvisaker <eylvisaker@gmail.com>
2014-07-26 18:44:03 +0000

    Make SmokedGB build with AgateLib 2.0.
    git-svn-id: svn://192.168.0.200/SmokedGB/trunk@6 a294bd8d-308b-4eb9-8dcc-7668ce4806f5

--- SmokedGBSharp/SmokedGB.cs
+++ SmokedGBSharp/SmokedGB.cs
@@ -6,6 +6,7 @@ using AgateLib;
 using AgateLib.DisplayLib;
 using AgateLib.InputLib;
 using AgateLib.Geometry;
+using AgateLib.Platform.WindowsForms.ApplicationModels;
 
 namespace SmokedGBSharp
 {
@@ -15,12 +16,15 @@ namespace SmokedGBSharp
 		/// The main entry point for the application.
 		/// </summary>
 		[STAThread]
-		static void Main()
+		static void Main(string[] args)
 		{
 			Application.SetCompatibleTextRenderingDefault(true);
 			Application.EnableVisualStyles();
 
-			new SmokedGB().Run();
+			PassiveModel.Run(args, () =>
+			{
+				new SmokedGB().Run();
+			});
 		}
 
 		Gameboy gmb;
@@ -38,33 +42,25 @@ namespace SmokedGBSharp
 			mKeys[KeyCode.Z] = Button.B;
 			mKeys[KeyCode.X] = Button.A;
 
-			using (AgateLib.AgateSetup setup = new AgateLib.AgateSetup())
+			frmSmoked frm = new frmSmoked();
+
+			DisplayWindow wind = DisplayWindow.CreateFromControl(frm.RenderTarget);
+
+			Keyboard.KeyDown += new InputEventHandler(Keyboard_KeyDown);
+			Keyboard.KeyUp += new InputEventHandler(Keyboard_KeyUp);
+			gmb = new Gameboy();
+			frm.Show();
+			frm.Gmb = gmb;
+
+			while (frm.Done == false)
 			{
-				//setup.AskUser = true;
-				setup.InitializeAll();
-				if (setup.WasCanceled)
-					return;
+				Display.BeginFrame();
+				Display.Clear(Color.Blue);
 
-				frmSmoked frm = new frmSmoked();
+				gmb.DrawScreen();
 
-				DisplayWindow wind = DisplayWindow.CreateFromControl(frm.RenderTarget);
-				
-				Keyboard.KeyDown += new InputEventHandler(Keyboard_KeyDown);
-				Keyboard.KeyUp += new InputEventHandler(Keyboard_KeyUp);
-				gmb = new Gameboy();
-				frm.Show();
-				frm.Gmb = gmb;
-
-				while (frm.Done == false)
-				{
-					Display.BeginFrame();
-					Display.Clear(Color.Blue);
-
-					gmb.DrawScreen();
-
-					Display.EndFrame();
-					Core.KeepAlive();
-				}
+				Display.EndFrame();
+				Core.KeepAlive();
 			}
 		}
 
@@ -94,9 +90,9 @@ namespace SmokedGBSharp
 				gmb.SetButtonState(mKeys[e.KeyCode], true);
 			}
 
-			switch(e.KeyCode)
+			switch (e.KeyCode)
 			{
-				case KeyCode.Tilde:				
+				case KeyCode.Tilde:
 					gmb.Cpu.LimitSpeed = false;
 					break;
 			}
