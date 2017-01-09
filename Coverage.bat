8f53ba787999bbcd67873d04f9e3b4552ae31dda
Erik Ylvisaker <eylvisaker@gmail.com>
2017-01-08 23:16:58 -0800

    Add -register:user to coverage.bat.

--- Coverage.bat
+++ Coverage.bat
@@ -1,1 +1,1 @@
-OpenCover.Console.exe -target:MSTest.exe -targetargs:/testcontainer:SmokedGB.UnitTests\bin\Debug\SmokedGB.UnitTests.dll -targetargs:/noisolation -excludebyfile:*\*.Designer.cs -output:Coverage.xml
+OpenCover.Console.exe -target:MSTest.exe -targetargs:/testcontainer:SmokedGB.UnitTests\bin\Debug\SmokedGB.UnitTests.dll -targetargs:/noisolation -excludebyfile:*\*.Designer.cs -output:Coverage.xml -register:user
