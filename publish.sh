6d98f35abf99c26f87bd63bea4d0a3105d241832
Erik Ylvisaker <eylvisaker@gmail.com>
2018-09-30 01:31:08 +0000

    Set correct directory for ftp upload

--- publish.sh
+++ publish.sh
@@ -9,4 +9,5 @@ dest=$FTP_DEST
 echo "Uploading to ftp://$FTP_REMOTE_HOST/$FTP_DEST"
 echo "Logging in as $FTP_USERNAME"
 
-ftp-upload -h $FTP_REMOTE_HOST -u $FTP_USERNAME --password $FTP_PASSWORD -d $FTP_DEST $1/*Windows*.zip
+ftp-upload -h $FTP_REMOTE_HOST -u $FTP_USERNAME --password $FTP_PASSWORD -d SmokedGB $1/*Windows*.zip
+
